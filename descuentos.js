41eb657c309d34145a87521eef67e18729dbb076
yannych1995 <yannymaly@gmal.com>
2024-05-29 11:30:05 -0500

    Ingresando Cupones al precio de un producto

--- descuentos.js
+++ descuentos.js
@@ -1,11 +1,12 @@
 const inputPrice = document.querySelector ('#price');
-const inputDiscount=document.querySelector('#discount');
+//const inputDiscount=document.querySelector('#discount');
+const inputCoupon=document.querySelector('#coupon')
 const btn = document.querySelector ('#calcular');
 const pResult=document.querySelector('#result');
 
-btn.addEventListener ('click', calcularPrecioConDescuento);
+btn.addEventListener ('click', calcularPrecioConCupon);
 
-function calcularPrecioConDescuento(){
+/*function calcularPrecioConDescuento(){
     //(P*(100-D)) /100
     const price= Number(inputPrice.value);
     const discount= Number(inputDiscount.value);
@@ -25,6 +26,48 @@ function calcularPrecioConDescuento(){
 
     pResult.innerHTML= 'El nuevo precio con descuento es  $'+ newPrice;
 
+}*/
+
+function calcularPrecioConCupon(){
+    const price= Number(inputPrice.value);
+    const coupon= inputCoupon.value;
+
+    if(!price || !coupon){
+        pResult.innerText= 'Por favor, ingresa valores validos al formulario';
+        return;
+    }
+
+    let discount;
+
+    switch (coupon) {
+        case 'Luli_2024':
+            discount=30;
+            break;
+        case 'Azra_2025':
+            discount=25;
+            break;
+        default:
+            pResult.innerText= 'El Cupon no es Valido';
+            return;
+        
+    }
+
+    /*if(coupon == 'Luli_2024'){
+        discount = 30;
+
+    }else if (coupon == 'Azra_2025'){
+        discount = 25;
+    }else{
+        pResult.innerText= 'El Cupon no es Valido';
+        return;
+    }*/
+
+    const newPrice= (price * (100-discount)) /100;
+
+    pResult.innerHTML= 'El nuevo precio con descuento es  $'+ newPrice;
+
 }
+
+
 
 
