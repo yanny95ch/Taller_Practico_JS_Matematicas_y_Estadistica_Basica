e0ab767176e40d0a690ac36bce9484fa8c0b3f4e
yannych1995 <yannymaly@gmal.com>
2024-07-09 17:31:58 -0500

    Como llegar a calcular la moda

--- Promedio-Media-Moda.js
+++ Promedio-Media-Moda.js
@@ -94,6 +94,23 @@ function ordenarLista(listaDesordenada){
     return lista;
 
 }
+//----------------------------------------------------------
+//Calcular Moda
+
+function calcularModa (lista){
+    const listaCount = {};
+        for (let i = 0; i< lista.length; i++) {
+            const elemento = lista[i];
+
+            if (listaCount[elemento]) {
+                listaCount[elemento] += 1;
+            }else {
+                listaCount[elemento] = 1;
+            }
+        }
+
+    console.log(listaCount);
+}
 
 
 
