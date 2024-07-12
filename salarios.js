41dd2bd03719271ac02db6cfdb5bea45ff7f03ee
yannych1995 <yannymaly@gmal.com>
2024-07-12 11:30:06 -0500

    Reestructurando información sobre la marcha

--- salarios.js
+++ salarios.js
@@ -50,3 +50,31 @@ function proyeccionPorPersona(nombrePersona){
 
      console.log({nuevosalario});
 }
+
+// ANALISIS empresasRIA
+/* 
+{
+    Industrias Mokepon:{
+        2018: [salario,salarios,salarios];
+        2019: [];
+        2020:
+        2025:
+    },
+}
+*/
+const empresas = {};
+for (persona of salarios){
+    for (trabajo of persona.trabajos){
+        if(!empresas[trabajo.empresa]){
+            empresas[trabajo.empresa]= {};
+        };
+
+        if(!empresas[trabajo.empresa][trabajo.year]){
+            empresas[trabajo.empresa][trabajo.year] = [];
+        }
+
+        empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+    }
+}
+console.log({empresas});
+
